e917ba1dc3aebb527dd8f04204a2814b0bb9159e
Theguyhere <mxlin01@gmail.com>
2022-06-09 09:33:51 -0700

    Fully added PAPI into Maven

---
+++ plugin/src/main/java/me/theguyhere/villagerdefense/plugin/papiExpansions/VDPlaceholderExpansion.java
@@ -0,0 +1,21 @@
+package me.theguyhere.villagerdefense.plugin.papiExpansions;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.jetbrains.annotations.NotNull;
+
+public class VDPlaceholderExpansion extends PlaceholderExpansion {
+    @Override
+    public @NotNull String getIdentifier() {
+        return "Theguyhere";
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return null;
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+}
