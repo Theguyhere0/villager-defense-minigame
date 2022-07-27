5ccf458971742a02490944c8b29552d76d7bb73f
Theguyhere <mxlin01@gmail.com>
2022-07-26 19:41:39 -0700

    Prep metadata for release

--- plugin/src/main/java/me/theguyhere/villagerdefense/plugin/Main.java
+++ plugin/src/main/java/me/theguyhere/villagerdefense/plugin/Main.java
@@ -46,7 +46,7 @@ public class Main extends JavaPlugin {
 
 	// Global state variables
 	private static boolean outdated = false; // DO NOT CHANGE
-	public static final boolean releaseMode = false;
+	public static final boolean releaseMode = true;
 	public static final int configVersion = 9;
 	public static final int arenaDataVersion = 8;
 	public static final int playerDataVersion = 3;
