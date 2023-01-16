08646930b34377997bd0d8bc76bf6a9d9feff4b5
Theguyhere <mxlin01@gmail.com>
2023-01-16 15:55:50 -0700

    1.19.3 support

---
+++ nms/v1_19_r2/src/main/java/me/theguyhere/villagerdefense/nms/v1_19_r2/EntityDestroyPacket.java
@@ -0,0 +1,29 @@
+package me.theguyhere.villagerdefense.nms.v1_19_r2;
+
+import me.theguyhere.villagerdefense.nms.common.EntityID;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.PacketPlayOutEntityDestroy;
+
+/**
+ * Packet class for destroying entities.
+ *
+ * This class format was borrowed from filoghost.
+ */
+class EntityDestroyPacket extends VersionNMSPacket {
+    private final Packet<?> rawPacket;
+
+    EntityDestroyPacket(EntityID entityID) {
+        PacketSetter packetSetter = PacketSetter.get();
+
+        // Write ID of entity to destroy
+        packetSetter.writeVarIntArray(entityID.getNumericID());
+
+        // Send out packet
+        rawPacket = new PacketPlayOutEntityDestroy(packetSetter);
+    }
+
+    @Override
+    Packet<?> getRawPacket() {
+        return rawPacket;
+    }
+}
