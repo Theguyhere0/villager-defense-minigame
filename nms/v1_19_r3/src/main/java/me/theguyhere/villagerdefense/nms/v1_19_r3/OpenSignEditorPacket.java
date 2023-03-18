27f17f6999f94f4be87f8d3d745d22ab9afe13de
Theguyhere <mxlin01@gmail.com>
2023-03-17 17:48:43 -0700

    1.19.4 support

---
+++ nms/v1_19_r3/src/main/java/me/theguyhere/villagerdefense/nms/v1_19_r3/OpenSignEditorPacket.java
@@ -0,0 +1,23 @@
+package me.theguyhere.villagerdefense.nms.v1_19_r3;
+
+import net.minecraft.core.BlockPosition;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.PacketPlayOutOpenSignEditor;
+
+public class OpenSignEditorPacket extends VersionNMSPacket {
+    private final Packet<?> rawPacket;
+
+    OpenSignEditorPacket(BlockPosition position) {
+        PacketSetter packetSetter = PacketSetter.get();
+
+        // Sign location
+        packetSetter.writePosition(position);
+
+        rawPacket = new PacketPlayOutOpenSignEditor(packetSetter);
+    }
+
+    @Override
+    public Packet<?> getRawPacket() {
+        return rawPacket;
+    }
+}
