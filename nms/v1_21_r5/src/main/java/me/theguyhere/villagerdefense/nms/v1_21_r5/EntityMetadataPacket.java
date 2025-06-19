e0650b9e3b20c40eab4f6324f82b1c83608b714d
datatags <datatags@users.noreply.github.com>
2025-06-19 15:16:47 -0700

    Add 1.21.6 support

---
+++ nms/v1_21_r5/src/main/java/me/theguyhere/villagerdefense/nms/v1_21_r5/EntityMetadataPacket.java
@@ -0,0 +1,42 @@
+package me.theguyhere.villagerdefense.nms.v1_21_r5;
+
+import me.theguyhere.villagerdefense.nms.common.EntityID;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundSetEntityDataPacket;
+import net.minecraft.server.MinecraftServer;
+
+/**
+ * A class for sending entity metadata packets.
+ */
+@SuppressWarnings("deprecation")
+class EntityMetadataPacket extends VersionNMSPacket {
+    private final Packet<?> rawPacket;
+
+    private EntityMetadataPacket(PacketSetter packetSetter) {
+        rawPacket = ClientboundSetEntityDataPacket.STREAM_CODEC.decode(new RegistryFriendlyByteBuf(packetSetter, MinecraftServer
+            .getServer().registryAccess()));
+    }
+
+    @Override
+    Packet<?> getRawPacket() {
+        return rawPacket;
+    }
+
+    public static DataWatcherPacketBuilder<EntityMetadataPacket> builder(EntityID entityID) {
+        PacketSetter packetSetter = PacketSetter.get();
+        packetSetter.writeVarInt(entityID.getNumericID());
+        return new Builder(packetSetter);
+    }
+
+    private static class Builder extends DataWatcherPacketBuilder<EntityMetadataPacket> {
+        private Builder(PacketSetter packetSetter) {
+            super(packetSetter);
+        }
+
+        @Override
+        EntityMetadataPacket createPacket(PacketSetter packetSetter) {
+            return new EntityMetadataPacket(packetSetter);
+        }
+    }
+}
